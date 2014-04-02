4557f8d7b5e2c0adc79ed0e938400cd69a40f7ec
Patrick Drechsler <patrick@pdrechsler.de>
2014-04-02 22:49:59 +0200

    why

---
+++ MockingStaticCreate/App/Feedback.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MockingStaticCreate.App
+{
+    public class Feedback
+    {
+        private int? quality;
+
+        /// <summary>
+        ///     Value in percentage (between 0 and 100).
+        /// </summary>
+        public int? Quality 
+        { 
+            get { return quality; } 
+            set
+            {
+                if (value < 0 || value > 100) {
+                    throw new ArgumentOutOfRangeException("value", "value must be between 0 and 100!");
+                }
+                quality = value; 
+            } 
+        }
+    }
+}
--- MockingStaticCreate/Tests/SomeServiceTests.cs
+++ MockingStaticCreate/Tests/SomeServiceTests.cs
@@ -1,21 +1,22 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using FakeItEasy;
 using FluentAssertions;
 using MockingStaticCreate.App;
 using Ploeh.AutoFixture;
 using Ploeh.AutoFixture.AutoFakeItEasy;
+using Ploeh.AutoFixture.Kernel;
 using Xunit;
 
 namespace MockingStaticCreate.Tests
 {
     public class SomeServiceTests
     {
-        [Fact]
+        [Fact(Skip = "Fails with: FakeItEasy.Core.FakeCreationExceptionFailed to create fake of type \"Testing.User\"")]
         public void HaveSameId_ForDifferentUsers_Should_ReturnFalse_UsingFakeItEasy()
         {
-            var user1 = A.Fake<User>();
-                // FakeItEasy.Core.FakeCreationExceptionFailed to create fake of type "Testing.User".
+            var user1 = A.Fake<User>(); // Fails here
             A.CallTo(() => user1.Id).Returns(1);
             var user2 = A.Fake<User>();
             A.CallTo(() => user2.Id).Returns(2);
@@ -26,7 +27,7 @@ namespace MockingStaticCreate.Tests
             result.Should().BeFalse();
         }
 
-        [Fact]
+        [Fact(Skip = "throws ArgumentNullException")]
         public void HaveSameId_ForDifferentUsers_Should_ReturnFalse_UsingCreate()
         {
             IFixture fixture = new Fixture().Customize(new AutoFakeItEasyCustomization());
@@ -38,7 +39,7 @@ namespace MockingStaticCreate.Tests
             result.Should().BeFalse();
         }
 
-        [Fact]
+        [Fact(Skip = "prop is read-only")]
         public void HaveSameId_ForDifferentUsers_Should_ReturnFalse_UsingBuild()
         {
             IFixture fixture = new Fixture().Customize(new AutoFakeItEasyCustomization());
@@ -51,7 +52,7 @@ namespace MockingStaticCreate.Tests
             result.Should().BeFalse();
         }
 
-        [Fact]
+        [Fact(Skip = "throws ArgumentNullException")]
         public void HaveSameId_ForDifferentUsers_Should_ReturnFalse_UsingFactory()
         {
             IFixture fixture = new Fixture().Customize(new AutoFakeItEasyCustomization());
@@ -63,11 +64,31 @@ namespace MockingStaticCreate.Tests
             result.Should().BeFalse();
         }
 
-        [Fact(Skip = "??")]
-        public void HaveSameId_ForDifferentUsers_Should_ReturnFalse_UsingFactoryWithFakeItEasy()
+        [Fact /*(Skip = "??")*/]
+        public void CreatingUserWithId_Should_Work_UsingFactoryAndSpecimenBuilder()
         {
             IFixture fixture = new Fixture().Customize(new AutoFakeItEasyCustomization());
-            // ???
+            fixture.Customizations.Add(new UserBuilder());
+            fixture.Customize<User>(o => o.FromFactory(() => User.Create("foo")));
+
+            var user = fixture.Create<User>();
+
+            user.Should().NotBeNull();
+            user.Id.Should().HaveValue();
+        }
+    }
+
+    public class UserBuilder : ISpecimenBuilder
+    {
+        public object Create(object request, ISpecimenContext context)
+        {
+            var pi = request as PropertyInfo;
+            if (pi == null) return new NoSpecimen(request);
+
+            // The following code only works for public properties... :-(
+            if (pi.Name == "Id" && pi.PropertyType == typeof (long?)) return 42;
+
+            return new NoSpecimen(request);
         }
     }
 }
